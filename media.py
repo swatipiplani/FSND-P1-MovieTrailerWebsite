0c29ea8151edc40e6684f9f6765e2990c9ab211f
swati piplani <swatidpiplani@gmail.com>
2017-01-18 00:41:31 +0530

    Code review finding implemented

--- media.py
+++ media.py
@@ -1,8 +1,11 @@
 '''File handles all the work related to Movie Class'''
 import webbrowser
+
+
 class Movie():
     '''Movie Class'''
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         '''Method to initialise Movie Class
                Parameters:
                movie_title (str): represent Movie Title
@@ -14,6 +17,7 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
     def show_trailer(self):
         '''Show Movie Trailer'''
         webbrowser.open(self.trailer_youtube_url)
